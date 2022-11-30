7cdf55fe1eb54aa358a29c989bb535d549d3d598
bitbytejoy <bitbytejoy@gmail.com>
2022-11-30 15:40:11 +0100

    init: BIG BANG

---
+++ src/main/java/de/neuefische/Main.java
@@ -0,0 +1,11 @@
+package de.neuefische;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void iFail () {
+        throw new ArithmeticException("asdf");
+    }
+}
---
+++ src/test/java/de/neuefische/MainTest.java
@@ -0,0 +1,21 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void iFail_whenCalled_throwsArithmeticExceptionWithAsdf() {
+//        Assertions.assertThrows(ArithmeticException.class, () -> {
+//            Main.iFail()
+//        });
+
+        try {
+            Main.iFail();
+            Assertions.fail();
+        } catch (ArithmeticException e) {
+            Assertions.assertTrue(true);
+        }
+    }
+}
